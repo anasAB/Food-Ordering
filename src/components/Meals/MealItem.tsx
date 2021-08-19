d040e34aec06656a84d413bdd5e7185181d2c402
anasAB <anas.222@hotmail.com>
2021-08-19 09:55:29 +0200

    - adding popup ingredients    - move the InfoIcon into Model    - change the styling    - adding ingredients data     (i) src/components/Cart/InfoIcon.tsx     (i) src/components/Layout/Backdrop.tsx     (i) src/components/Layout/Header.css     (i) src/components/Layout/Header.tsx     (i) src/components/Layout/Model.css     (i) src/components/Layout/Model.tsx     (i) src/components/Meals/AvailableMeals.tsx     (i) src/components/Meals/MealItem.css     (i) src/components/Meals/MealItem.tsx

--- src/components/Meals/MealItem.tsx
+++ src/components/Meals/MealItem.tsx
@@ -1,8 +1,3 @@
-// import React,{Component} from 'react'
-import React, { useState } from 'react'
-
-import InfoIcon from '../Cart/InfoIcon'
-import Backdrop from '../Layout/Backdrop'
 import Model from '../Layout/Model'
 import './MealItem.css'
 
@@ -11,29 +6,7 @@ import './MealItem.css'
 
 const MealItem = (props) => {
     const { meal } = props
-    const [modalOpen, setModalOpenSet] = useState(false)
-
-
-    const OpenModalHandler = (ModalState) => {
-        setModalOpenSet(ModalState)
-    }
-
-    const CloseModalHandler = (ModalState) => {
-        setModalOpenSet(ModalState)
-    }
-
-    const modal = modalOpen ?
-        <Backdrop>
-            <Model 
-            show={modalOpen} 
-            closeModal={OpenModalHandler}
-            mealInfo={meal}
-             />
-        </Backdrop> :
-        ''
-
     return (
-        
         <div className="container meal">
             <div className="row">
                 <div className="col ">
@@ -45,10 +18,11 @@ const MealItem = (props) => {
                 <div className="col price">
                     ${meal.price}
                 </div>
-                <InfoIcon InfoIcon={meal} modalOpenState={modalOpen} handleOpenModal={OpenModalHandler} />
-                {modal}
-
-
+                <div className="col">
+                    <Model
+                        mealInfo={meal}
+                    />
+                </div>
             </div>
         </div>
     )
