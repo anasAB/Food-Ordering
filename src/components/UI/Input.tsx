8efe3f3e205760c3343f1d70ed7ce094cf838690
anasAB <anas.222@hotmail.com>
2021-11-05 15:12:54 +0100

    refactor MealItemForm by removing the input

--- src/components/UI/Input.tsx
+++ src/components/UI/Input.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 import './Input.css'
 
 const Input = React.forwardRef((props,ref) => {
+    console.log('props',props);
     return (
         <div className='input'>
-            <label htmlFor={props.input.id} /*value={props.userInput.enteredTitle}*/>{props.label}</label>
+            <label htmlFor={props.id} /*value={props.userInput.enteredTitle}*/>{props.label}</label>
             <input ref={ref} {...props.input} />
         </div>
     )
