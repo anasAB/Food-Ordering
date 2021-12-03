069f5297ac92033f39c690174590f1e225987bce
anasAB <anas.222@hotmail.com>
2021-12-03 12:10:08 +0100

    (i) src/components/Layout/MealInfo.tsx (i) src/components/Meals/AvailableMeals.css (i) src/components/Meals/AvailableMeals.tsx - refactor AvailableMeals component
    (i) src/components/Meals/MealItem.css
    (i) src/components/Meals/MealItem.tsx
    (i) src/components/UI/HomePage.css
     - refactor the css file
      - refactor the return of the component

--- src/components/Meals/AvailableMeals.tsx
+++ src/components/Meals/AvailableMeals.tsx
@@ -6,18 +6,15 @@ import MealItem from './MealItem';
 
 const AvailableMeals = (props) => {
     let { meals } = props;
+
     return (
-        <div className='meals' >
-            <div className="container">
-                <div className="row">
-                    {meals.map(meal => (
-                        <div className="col-sm meal_item" key={meal.id} >
-                            <MealItem
-                                meal={meal}
-                            />
-                        </div>
-                    ))}
-                </div>
+        <div className="container availableMeals">
+            <div className="row row-cols-2">
+                {meals.map(meal => (
+                    <div className="col-12 col-lg-6 mealItem">
+                        <MealItem meal={meal} />
+                    </div>
+                ))}
             </div>
         </div>
     )
